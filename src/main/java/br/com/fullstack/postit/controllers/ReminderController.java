7c1fa742f4aee5b50050fc68552cc1a9ddcf09ff
Gabriel Augustin <gabriel.augustin@edu.sc.senai.br>
2024-09-11 20:26:30 -0300

    COMPLETED: DTOs; Logs

--- src/main/java/br/com/fullstack/postit/controllers/ReminderController.java
+++ src/main/java/br/com/fullstack/postit/controllers/ReminderController.java
@@ -4,11 +4,13 @@ import br.com.fullstack.postit.dtos.ReminderRequest;
 import br.com.fullstack.postit.dtos.ReminderResponse;
 import br.com.fullstack.postit.services.ReminderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("reminders")
@@ -18,18 +20,30 @@ public class ReminderController {
 
     @GetMapping
     public Page<ReminderResponse> get(Pageable pageable) {
-        return service.findAll(pageable);
+        log.info("GET /reminders -> Begin");
+        Page<ReminderResponse> response = service.findAll(pageable);
+
+        log.info("GET /reminders -> End");
+        return response;
     }
 
     @GetMapping("{id}")
     public ReminderResponse getById(@PathVariable Long id) {
-        return service.findById(id);
+        log.info("GET /reminders/{} -> Begin", id);
+        ReminderResponse response = service.findById(id);
+
+        log.info("GET /reminders/{} -> End", id);
+        return response;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ReminderResponse post(@RequestBody ReminderRequest reminder) {
-        return service.create(reminder);
+        log.info("POST /reminders -> Begin");
+        ReminderResponse response = service.create(reminder);
+
+        log.info("POST /reminders -> End");
+        return response;
     }
 
     @PutMapping("{id}")
@@ -37,12 +51,18 @@ public class ReminderController {
             @PathVariable Long id,
             @RequestBody ReminderRequest reminder
     ) {
-        return service.update(id, reminder);
+        log.info("PUT /reminders/{} -> Begin", id);
+        ReminderResponse response = service.update(id, reminder);
+
+        log.info("PUT /reminders/{} -> End", id);
+        return response;
     }
 
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
+        log.info("DELETE /reminders/{} -> Begin", id);
         service.delete(id);
+        log.info("DELETE /reminders/{} -> End", id);
     }
 }
--- src/main/java/br/com/fullstack/postit/services/ReminderServiceImpl.java
+++ src/main/java/br/com/fullstack/postit/services/ReminderServiceImpl.java
@@ -20,30 +20,39 @@ public class ReminderServiceImpl implements ReminderService {
     @Override
     public ReminderResponse create(ReminderRequest request) {
         Reminder reminder = new Reminder(request);
-        repository.save(reminder);
-        return new ReminderResponse(reminder);
+        return save(reminder);
     }
 
     @Override
     public Page<ReminderResponse> findAll(Pageable pageable) {
-        return repository.findAll(pageable);
+        return repository.findAll(pageable).map(ReminderResponse::new);
     }
 
     @Override
     public ReminderResponse findById(Long id) {
-        return repository.findById(id).orElseThrow(() -> new ReminderNotFound(id));
+        Reminder reminder = findEntityById(id);
+        return new ReminderResponse(reminder);
     }
 
     @Override
-    public ReminderResponse update(Long id, ReminderRequest reminder) {
-        findById(id);
-        reminder.setId(id);
-        return repository.save(reminder);
+    public ReminderResponse update(Long id, ReminderRequest request) {
+        Reminder reminder = findEntityById(id);
+        BeanUtils.copyProperties(request, reminder);
+        return save(reminder);
     }
 
     @Override
     public void delete(Long id) {
-        findById(id);
+        findEntityById(id);
         repository.deleteById(id);
+    }
+
+    private Reminder findEntityById(Long id) {
+        return repository.findById(id).orElseThrow(() -> new ReminderNotFound(id));
+    }
+
+    private ReminderResponse save(Reminder reminder) {
+        repository.save(reminder);
+        return new ReminderResponse(reminder);
     }
 }
